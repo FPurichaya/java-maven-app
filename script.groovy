00214949de1470b21f27abe8fe7964b3a8a376da
FPurichaya <56637153+FPurichaya@users.noreply.github.com>
2026-03-02 00:12:09 +0700

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -6,6 +6,6 @@ def testApp() {
 }
 def deployApp() {
     echo 'deploying the application...'
-    echo "Deploying version ${this.params.VERSION}"
+    echo "Deploying version ${params.VERSION}"
 }
 return this
