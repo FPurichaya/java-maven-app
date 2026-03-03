9e5dce3bd691b535bc1ef9e3568c75355c18043e
FPurichaya <56637153+FPurichaya@users.noreply.github.com>
2026-03-03 22:48:42 +0700

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -5,12 +5,13 @@ def buildJar() {
 
 def buildImage() {
     echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER' )]) 
+    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER' )]) { 
     sh 'docker build -t fpurichaya/demo-app:jma-2.0 .'
     sh 'echo $PASS | docker login -u $USER --password-stdin'
     sh 'docker push fpurichaya/demo-app:jma-2.0'
+    }
 }
 
 def deployApp() {
-    echo 'deplying the application...'
+    echo 'deploying the application...'
 }
